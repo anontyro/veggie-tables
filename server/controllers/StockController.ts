3393dff159ab87ab8db6d4afb6a7fb194400038f
Alexander Wilkinson <alexwilkinson@gmail.com>
2020-04-02 23:54:22 +0800

    added in the basic CRUD operations on the backend to allow items to be manipulated next will be working on the frontend

--- server/controllers/StockController.ts
+++ server/controllers/StockController.ts
@@ -1,5 +1,5 @@
 import { OK, BAD_REQUEST } from 'http-status-codes';
-import { Controller, Get } from '@overnightjs/core';
+import { Controller, Get, Post, Delete } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
 import { Request, Response } from 'express';
 import connection from '../connection';
@@ -7,6 +7,24 @@ import Stock from '../database/stock/entity';
 
 @Controller('api/stock/')
 class StockController {
+  @Get()
+  private getAllItems(req: Request, res: Response) {
+    try {
+      connection.then(async connection => {
+        const itemList: Stock[] = await connection.manager.find(Stock);
+        return res.status(OK).json({
+          response: itemList,
+        });
+      });
+    } catch (err) {
+      Logger.Err(err, true);
+      return res.status(BAD_REQUEST).json({
+        error: err.message,
+        response: {},
+      });
+    }
+  }
+
   @Get(':id')
   private getById(req: Request, res: Response) {
     try {
@@ -14,7 +32,7 @@ class StockController {
         const { id } = req.params;
         const item: Stock = await connection.manager.findOne(Stock, {
           where: {
-            id: id,
+            id,
           },
         });
         return res.status(OK).json({
@@ -25,6 +43,53 @@ class StockController {
       Logger.Err(err, true);
       return res.status(BAD_REQUEST).json({
         error: err.message,
+        response: {},
+      });
+    }
+  }
+
+  @Post()
+  private addItem(req: Request, res: Response) {
+    try {
+      connection.then(async connection => {
+        const item: Stock = req.body;
+        const nextItem = connection.manager.create(Stock, item);
+        const results = await connection.manager.save(nextItem);
+        return res.status(OK).json({
+          response: results,
+        });
+      });
+    } catch (err) {
+      Logger.Err(err, true);
+      return res.status(BAD_REQUEST).json({
+        error: err.message,
+        response: {},
+      });
+    }
+  }
+
+  @Delete(':id')
+  private removeItem(req: Request, res: Response) {
+    try {
+      connection.then(async connection => {
+        const { id } = req.params;
+        const item: Stock = await connection.manager.findOne(Stock, {
+          where: {
+            id,
+          },
+        });
+        const results = await connection.manager.delete(Stock, id);
+        const removed = results.raw.affectedRows > 0;
+        return res.status(OK).json({
+          removed,
+          response: item,
+        });
+      });
+    } catch (err) {
+      Logger.Err(err, true);
+      return res.status(BAD_REQUEST).json({
+        error: err.message,
+        response: {},
       });
     }
   }
