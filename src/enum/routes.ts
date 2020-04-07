9d1ac0543c5c0b1fc86e9d5aac53315271abed5f
alexWilkinson <alex@hooq.tv>
2020-04-07 09:52:21 +0800

    changed colour type to ts and added in frontend routes

--- src/enum/routes.ts
+++ src/enum/routes.ts
@@ -1,3 +1,12 @@
 export const BACKEND_ROUTES = {
   STOCK_ROOT: '/api/stock',
 };
+
+export const FRONTEND_ROUTES = {
+  HOME: '/',
+  CART: '/cart',
+  ADMIN: {
+    ITEM_LIST: '/admin/item-list',
+    ADD_ITEM: '/add-item',
+  },
+};
